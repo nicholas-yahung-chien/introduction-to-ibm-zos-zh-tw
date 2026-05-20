2ce201d24e351b80c3549171c5c7ee1416784825
Nicholas Yahung Chien <nicholas.yahung.chien@gmail.com>
2026-05-20 23:26:36 +0800

    Tighten first edition content quality checks

--- docs/.vitepress/theme/components/VideoLesson.vue
+++ docs/.vitepress/theme/components/VideoLesson.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
 }>()
 
 const mediaVersion = 'hls-f6d6b21'
-const subtitleVersion = 'subtitles-semantic-sweep'
+const subtitleVersion = 'subtitles-quality-wrap'
 const mediaBaseUrl = import.meta.env.VITE_MEDIA_BASE_URL?.replace(/\/$/, '')
 
 function appendVersion(src: string, version = mediaVersion) {
